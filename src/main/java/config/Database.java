1f09c70ee1263c24affa86866048677fd2a9869f
michal <michal.zurek7@gmail.com>
2016-03-05 21:37:01 +0100

    dcr

--- src/main/java/config/Database.java
+++ src/main/java/config/Database.java
@@ -1,18 +1,29 @@
 package config;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.hibernate3.annotation.AnnotationSessionFactoryBean;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 
 /**
  * Created by mzurek on 3/5/16.
  */
 @Configuration
+@EnableTransactionManagement
+//@ComponentScan({ "com.websystique.springmvc.configuration" })
+//@PropertySource(value = { "classpath:application.properties" })
 public class Database {
 
     @Bean
@@ -22,5 +33,32 @@ public class Database {
                 .setType(EmbeddedDatabaseType.HSQL) //.H2 or .DERBY
                 .build();
         return db;
+    }
+
+    @Bean
+    public SessionFactory sessionFactory() {
+        LocalSessionFactoryBuilder localSessionFactoryBuilder = new LocalSessionFactoryBuilder(dataSource());
+        localSessionFactoryBuilder.scanPackages("model.entities");
+        localSessionFactoryBuilder.addProperties(hibernateProperties());
+
+
+        SessionFactory sessionFactory = localSessionFactoryBuilder.buildSessionFactory();
+        return sessionFactory;
+    }
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory s) {
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(s);
+        return txManager;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", "");
+        properties.put("hibernate.show_sql", Boolean.TRUE.toString());
+        /properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+        return properties;
     }
 }
